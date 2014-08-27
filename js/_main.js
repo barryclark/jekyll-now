a0158f4cc6dde1a319d68101591a3cf1e1d37f3a
Michael Rose <est.michael@gmail.com>
2014-08-27 15:16:21 -0400

    Consolidate toggleClass functions for off canvas menu

--- js/_main.js
+++ js/_main.js
@@ -3,20 +3,16 @@
 $(document).ready(function(){
 	$('.js-menu-trigger').on('click touchstart', function(e){
 		$('body').toggleClass('no-scroll');
-		$('.js-menu').toggleClass('is-visible');
-		$('.js-menu-screen').toggleClass('is-visible');
+		$('.js-menu, .js-menu-screen').toggleClass('is-visible');
 		$('.sliding-menu-button').toggleClass('slide close');
-		$('#masthead').toggleClass('slide');
-		$('#page-wrapper').toggleClass('slide');
+		$('#masthead, #page-wrapper').toggleClass('slide');
 		e.preventDefault();
 	});
 	$('.js-menu-screen').on('click touchstart', function(e){
 		$('body').toggleClass('no-scroll');
-		$('.js-menu').toggleClass('is-visible');
-		$('.js-menu-screen').toggleClass('is-visible');
+		$('.js-menu, .js-menu-screen').toggleClass('is-visible');
 		$('.sliding-menu-button').toggleClass('slide close');
-		$('#masthead').toggleClass('slide');
-		$('#page-wrapper').toggleClass('slide');
+		$('#masthead, #page-wrapper').toggleClass('slide');
 		e.preventDefault();
 	});
 });
