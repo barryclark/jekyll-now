fd0125f0cfb62ee3943f1fa005aa0f8704bfd722
Florian Schaeffler <florian.schaeffler@mobilityhouse.com>
2015-05-20 21:07:04 +0200

    fixed deployment script

--- _deploy/_deploy.sh
+++ _deploy/_deploy.sh
@@ -24,26 +24,26 @@ aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --expires "'$expire_
 aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --expires "'$expire_1yr'" --cache-control 'max-age=6048000' --include '*.png' --content-type 'image/png'
 
 # sync all css
-aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=6048000, must-revalidate' --include '*.css' --content-type 'text/css'
+aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=6048000, must-revalidate' --include '*.css' --content-type 'text/css; charset=UTF-8'
 
 # snyc all javascript
-aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=6048000' --include '*.js' --content-type 'application/javascript'
+aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=6048000' --include '*.js' --content-type 'application/javascript; charset=UTF-8'
 
 # sync feed xml
-aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=7200, must-revalidate' --include '*feed.xml' --content-type 'application/rss+xml'
+aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=7200, must-revalidate' --include '*feed.xml' --content-type 'application/rss+xml; charset=UTF-8'
 
 # sync sitemap
-aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=7200, must-revalidate' --include '*sitemap.xml' --content-type 'application/xml'
+aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=7200, must-revalidate' --include '*sitemap.xml' --content-type 'application/xml; charset=UTF-8'
 
 # snyc html
-aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=7200, must-revalidate' --include '*.html' --include '*.htm' --content-type 'text/html'
+aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=7200, must-revalidate' --include '*.html' --include '*.htm' --content-type 'text/html; charset=UTF-8'
 
 # snyc txt
-aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=86400' --include '*.txt' --content-type 'text/plain'
+aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --exclude '*.*' --content-encoding 'gzip' --cache-control 'max-age=86400' --include '*.txt' --content-type 'text/plain; charset=UTF-8'
 
 # sync rest and delete unused elements
 aws s3 sync $SITE_FOLDER s3://$BUCKET_NAME/ --delete
 
-# invalidation_batch_param="'{\"Paths\": {\"Quantity\": 1,\"Items\": [\"/*\"]},\"CallerReference\": \"$TIMESTAMP\"}'"
-# invalidation_cmd="aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --invalidation-batch $invalidation_batch_param"
-# eval $invalidation_cmd
+invalidation_batch_param="'{\"Paths\": {\"Quantity\": 1,\"Items\": [\"/*\"]},\"CallerReference\": \"$TIMESTAMP\"}'"
+invalidation_cmd="aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --invalidation-batch $invalidation_batch_param"
+eval $invalidation_cmd
