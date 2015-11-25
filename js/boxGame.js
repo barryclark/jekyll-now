bf24a92984ec9117bd4488428d52b4ce03ffd304
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-25 12:52:25 -0500

    cleanup

--- js/boxGame.js
+++ js/boxGame.js
@@ -1,14 +1,12 @@
-function getMousePos(canvas, evt) 
-{
-    var rect = canvas.getBoundingClientRect();
+function getMousePos(canvas, evt) {
+	var rect = canvas.getBoundingClientRect();
     return {
         x: evt.clientX - rect.left, 
         y: evt.clientY - rect.top
     };
 }
 
-function getRandomColor()
-{
+function getRandomColor() {
 	var r = Math.round(Math.random() * 255);
     var g = Math.round(Math.random() * 255);
     var b = Math.round(Math.random() * 255);
@@ -16,80 +14,73 @@ function getRandomColor()
     return 'rgb(' + r +',' + g + ',' + b + ')';
 }
 
-function box(x,y,width,height)
-{
+function box(x,y,width,height) {
 	this.x = x;
 	this.y = y;
 	this.width = width;
 	this.height = height;
 	this.color = getRandomColor();
-	this.drawBox = function(ctx)
-					{
-						ctx.fillStyle=this.color;
-						ctx.fillRect(this.x,this.y,this.width,this.height);
-					};
-	this.isPointInBox = function(x,y)
-					{
-						if(x < this.x)
-							return false;
-						if(y < this.y)
-							return false;
-						if(x > this.x + this.width)
-							return false;
-						if(y > this.y + this.height)
-							return false;
-						
-						return true;
-					};
-	this.overLapsBox = function(box)
-					{
-						if(box.isPointInBox(this.x, this.y))
-							return true;
-						if(box.isPointInBox(this.x + this.width, this.y))
-							return true;
-						if(box.isPointInBox(this.x, this.y + this.height))
-							return true;
-						if(box.isPointInBox(this.x + this.width, this.y + this.height))
-							return true;
-						
-						return false;
-					};
-	this.overLapsBoxes = function(boxes)
-					{
-						for(var i = 0; i < boxes.length; i++)
-						{
-							if(boxes[i].overLapsBox(this))
-								return true;
-						}
-						
-						return false;
-					};
+	
+	this.drawBox = function(ctx) {
+		ctx.fillStyle=this.color;
+		ctx.fillRect(this.x,this.y,this.width,this.height);
+	};
+					
+	this.isPointInBox = function(x,y) {
+		if(x < this.x)
+			return false;
+		if(y < this.y)
+			return false;
+		if(x > this.x + this.width)
+			return false;
+		if(y > this.y + this.height)
+			return false;
+		
+		return true;
+	};
+					
+	this.overLapsBox = function(box) {
+		if(box.isPointInBox(this.x, this.y))
+			return true;
+		if(box.isPointInBox(this.x + this.width, this.y))
+			return true;
+		if(box.isPointInBox(this.x, this.y + this.height))
+			return true;
+		if(box.isPointInBox(this.x + this.width, this.y + this.height))
+			return true;
+		
+		return false;
+	};
+					
+	this.overLapsBoxes = function(boxes) {
+		for(var i = 0; i < boxes.length; i++) {
+			if(boxes[i].overLapsBox(this))
+				return true;
+		}
+		
+		return false;
+	};
 }
 
-function drawAllBoxes(boxes, ctx)
-{
+function drawAllBoxes(boxes, ctx) {
 	clearScreen(ctx);
 	
-	for(var i = 0; i < boxes.length; i++)
-	{
+	for(var i = 0; i < boxes.length; i++) {
 		boxes[i].drawBox(ctx);
 	}
 }
 
-function clearScreen(ctx)
-{
+function clearScreen(ctx) {
 	ctx.setTransform(1, 0, 0, 1, 0, 0);
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
-function clearArray(array)
-{
+function clearArray(array) {
 	while(array.length)
 		array.pop();
 }
 
-$(document).ready(function()
-{
+$(document).ready(function() {
 	var canvas = $("#canvas").get(0);
 	var ctx = canvas.getContext('2d');
 	var boxes = new Array();
@@ -99,14 +90,11 @@ $(document).ready(function()
 	
 	var time = 1000;
 		
-	$("#canvas").click(function(e)
-	{
+	$("#canvas").click(function(e) {
 		var mousePos = getMousePos(canvas,e);
 	    
-	    for(var i = boxes.length - 1; i >= 0; i--)
-    	{
-	    	if(boxes[i].isPointInBox(mousePos.x,mousePos.y))
-    		{
+	    for(var i = boxes.length - 1; i >= 0; i--) {
+	    	if(boxes[i].isPointInBox(mousePos.x,mousePos.y)) {
 	    		boxes.splice(i, 1);
 	    		drawAllBoxes(boxes, ctx);
 	    		score = score + 5;
@@ -136,8 +124,7 @@ $(document).ready(function()
 		$("#score").html(score.toString());
 	});
 	
-	$("#clearButton").click(function()
-	{
+	$("#clearButton").click(function() {
 		clearScreen(ctx);
 		clearArray(boxes);
 		score = 0;
@@ -145,23 +132,22 @@ $(document).ready(function()
 		time = 1000;
 	});
 	
-	var boxAdder = function()
-					{
-						var x = Math.round(Math.random() * canvas.width);
-						var y = Math.round(Math.random() * canvas.height);
-						
-						var newBox = new box(x,y,50,50);
-						newBox.drawBox(ctx);
-						
-					    if(newBox.overLapsBoxes(boxes))
-					    	score = score - 15;
-					    
-					    $("#score").html(score.toString());
-						
-						boxes.push(newBox);
-						
-						window.setTimeout(function(){boxAdder();}, time);
-					};
+	var boxAdder = function() {
+		var x = Math.round(Math.random() * canvas.width);
+		var y = Math.round(Math.random() * canvas.height);
+		
+		var newBox = new box(x,y,50,50);
+		newBox.drawBox(ctx);
+		
+		if(newBox.overLapsBoxes(boxes))
+			score = score - 15;
+		
+		$("#score").html(score.toString());
+		
+		boxes.push(newBox);
+		
+		window.setTimeout(function(){boxAdder();}, time);
+	};
 	
 	window.setTimeout(function(){boxAdder();},time);
 });
