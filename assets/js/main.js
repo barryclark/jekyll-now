869eb08e801a703812b5a329eacb24e5bd2f4456
Timur Catakli <timurcatakli@gmail.com>
2015-12-02 13:21:13 +0200

    Disable jquery rss feed

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,35 +22,35 @@ jQuery(document).ready(function($) {
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
     
-    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-    $("#rss-feeds").rss(
+    // /* jQuery RSS - https://github.com/sdepold/jquery-rss */
+    // $("#rss-feeds").rss(
     
-        //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
+    //     //Change this to your own rss feeds
+    //     "http://feeds.feedburner.com/TechCrunch/startups",
         
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 3,
+    //     {
+    //     // how many entries do you want?
+    //     // default: 4
+    //     // valid values: any integer
+    //     limit: 3,
         
-        // the effect, which is used to let the entries appear
-        // default: 'show'
-        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-        effect: 'slideFastSynced',
+    //     // the effect, which is used to let the entries appear
+    //     // default: 'show'
+    //     // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+    //     effect: 'slideFastSynced',
         
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='item'>{entries}</div>",
+    //     // outer template for the html transformation
+    //     // default: "<ul>{entries}</ul>"
+    //     // valid values: any string
+    //     layoutTemplate: "<div class='item'>{entries}</div>",
         
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
+    //     // inner template for each entry
+    //     // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+    //     // valid values: any string
+    //     entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
         
-        }
-    );
+    //     }
+    // );
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "timurcatakli", selector: "#ghfeed" });
