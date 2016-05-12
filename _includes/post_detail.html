d3d86dbc8f921e21a04d1197feb1f025944b0c84
Robert C. Kalajian Jr <rkalajian@gmail.com>
2016-05-11 20:54:39 -0400

    Test new layout

---
+++ _includes/post_detail.html
@@ -0,0 +1,9 @@
+<h1 class="entry-title">
+{% if page.title %}
+    <a href="{{ root_url }}{{ page.url }}">{{ page.title }}</a>
+{% endif %}
+{% if post.title %}
+    <a href="{{ root_url }}{{ post.url }}">{{ post.title }}</a>
+{% endif %}
+</h1>
+<div class="entry-content">{{ content }}</div>
--- _plugins/rssgenerator.rb
+++ _plugins/rssgenerator.rb
@@ -42,12 +42,12 @@ module Jekyll
         maker.channel.link = site.config['url']
         maker.channel.description = site.config['description'] || "RSS feed for #{site.config['name']}"
         maker.channel.author = site.config["author"]
-        maker.channel.updated = site.posts.map { |p| p.date  }.max
+        maker.channel.updated = site.posts.docs.map { |p| p.date  }.max
         maker.channel.copyright = site.config['copyright']
 
-        post_limit = site.config['rss_post_limit'].nil? ? site.posts.count : site.config['rss_post_limit'] - 1
+        post_limit = site.config['rss_post_limit'].nil? ? site.posts.docs.count : site.config['rss_post_limit'] - 1
 
-        site.posts.reverse[0..post_limit].each do |post|
+        site.posts.docs.reverse[0..post_limit].each do |post|
           post = post.dup
           post.render(site.layouts, site.site_payload)
           maker.items.new_item do |item|
