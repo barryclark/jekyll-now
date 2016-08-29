49c4b7aa8e157698698ff67db2fd5651b40b0330
Matteo Fortini <matteo.fortini@gmail.com>
2016-08-29 09:13:25 +0200

    Gestione contatti da foglio #91

--- scripts/csvupdate.sh
+++ scripts/csvupdate.sh
@@ -4,6 +4,7 @@ git clean -fxd
 git reset --hard HEAD
 git pull
 
+CSV_CONTATTI='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=0&single=true&output=csv'
 CSV_ALLOGGI='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=2016214992&single=true&output=csv'
 CSV_DONAZIONI='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=444688992&single=true&output=csv'
 CSV_RACCOLTE='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=178302385&single=true&output=csv'
@@ -11,6 +12,7 @@ CSV_NOTIZIE='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLe
 CSV_BOLLETTINO='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=814315&single=true&output=csv'
 CSV_FABBISOGNI='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=66190431&single=true&output=csv'
 
+wget -O _data/contatti.csv $CSV_CONTATTI
 wget -O _data/alloggi.csv $CSV_ALLOGGI 
 wget -O _data/donazioni.csv $CSV_DONAZIONI
 wget -O _data/raccolte.csv $CSV_RACCOLTE
