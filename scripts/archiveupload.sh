88883cfbb097797ca434295c24b09001a2d9ea7a
Timothy Redaelli <timothy.redaelli@gmail.com>
2016-08-29 16:27:58 +0200

    Aggiusto lo script per effettuare l'upload su archive.org
    Questa versione supporta pure nomi file contenti : o che cominciano
    con spazi dato che non si conosce la fonte dell'immagine e può essere
    qualsiasi cosa

--- scripts/archiveupload.sh
+++ scripts/archiveupload.sh
@@ -6,19 +6,15 @@
 # apt-get install libimage-exiftool-perl
 # pip install internetarchive (https://github.com/jjjake/internetarchive)
 gdrive download 0B3_1qNRcoeWseTlOejNUaFlpMjQ --recursive --force
-ls -R1 TerremotoCentroItalia_foto |
-  while read l; do case $l in *:) d=${l%:};; "") d=;; *)
-        FILE=$(echo "$d/$l")
+
+find "${1:-TerremotoCentroItalia_foto}" -type f -name '*.*' | while IFS= read -r FILE; do
+        d=$(dirname "$FILE")
         FILE_CLEAN=$(echo $FILE | tr "'" "_" | tr " " "_" | tr "," "_" | sed -r 's|\.|_|g; s|_([^_]*)$|.\1|')
-
-        if [[ ! -d "$FILE"  &&  $FILE == *"."*  ]]; then
-                FILE_BASE256=$(sha256sum "$FILE")
-                FILE_BASE256_EXT=$(echo $FILE_BASE256 | awk '{print $1}')".${FILE_CLEAN#*.}"
-                mv "$FILE" "$d/$FILE_BASE256_EXT"
-                jpegoptim "$d/$FILE_BASE256_EXT"
-                optipng "$d/$FILE_BASE256_EXT"
-                exiftool -m -UserComment="$FILE" "$d/$FILE_BASE256_EXT"
-                ia upload terremotocentroitaliainfo "$d/$FILE_BASE256_EXT" --retries 10
-        fi
-esac; done
-
+        FILE_BASE256=$(sha256sum "$FILE")
+        FILE_BASE256_EXT=$(echo $FILE_BASE256 | awk '{print $1}')".${FILE_CLEAN#*.}"
+        mv "$FILE" "$d/$FILE_BASE256_EXT"
+        jpegoptim "$d/$FILE_BASE256_EXT"
+        optipng "$d/$FILE_BASE256_EXT"
+        exiftool -m -UserComment="$FILE" "$d/$FILE_BASE256_EXT"
+        ia upload terremotocentroitaliainfo "$d/$FILE_BASE256_EXT" --retries 10
+done
