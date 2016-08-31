a55b2e84aee073baf199f0f545d81ab2d50a7c9d
tailot <tailot@gmail.com>
2016-08-31 13:14:56 +0200

    Implementato meccanismo di scrolling on top

--- js/main.js
+++ js/main.js
@@ -64,6 +64,8 @@ var main = {
     
     // show the big header image	
     main.initImgs();
+    //show scrolltop button
+    main.scrollTop();
   },
   
   initImgs : function() {
@@ -111,7 +113,37 @@ var main = {
 	  }
     }
   },
+
+  scrollTop : function() {
+    $(document).on( 'scroll', function(){
+   
+      if ($(window).scrollTop() > 100) {
+        $('.scroll-top-wrapper').addClass('show');
+      } else {
+        $('.scroll-top-wrapper').removeClass('show');
+      }
+    });
+   
+    $('.scroll-top-wrapper').on('click', function scrollToTop() {
+      verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+      element = $('body');
+      offset = element.offset();
+      offsetTop = offset.top;
+      $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+    });
+  },
+
+  getImgInfo : function() {
+    var randNum = Math.floor((Math.random() * main.numImgs) + 1);
+    var src = main.bigImgEl.attr("data-img-src-" + randNum);
+  var desc = main.bigImgEl.attr("data-img-desc-" + randNum);
   
+  return {
+    src : src,
+    desc : desc
+  }
+  },
+
   getImgInfo : function() {
   	var randNum = Math.floor((Math.random() * main.numImgs) + 1);
     var src = main.bigImgEl.attr("data-img-src-" + randNum);
