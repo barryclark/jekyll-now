73f1374f20b2b1b29c22b5e8551b394ffa1f5d44
hendrixjoseph <hendrixjoseph@aol.com>
2016-10-06 20:50:23 -0400

    new

---
+++ js/parseGet.js
@@ -0,0 +1,66 @@
+var isGetPresent() {
+	return location.search.length > 0;
+}
+
+var getArray = function() {
+	return location.search.substr(1).split('&');
+}
+
+var getKeyValuePairs = function() {
+	var array = getArray();
+	
+	var pairs = new Array();
+	
+	for(var i = 0; i < array.length; i++) {		
+		pairs.push(array[i].split('='));
+	}
+	
+	return pairs;
+}
+
+var getValueOfFirstKey = function(key) {
+	if(key !== null || key.length > 0) {
+		var array = getArray();
+		
+		for(var i = 0; i < array.length; i++) {		
+			var keyValue = array[i].split('=');
+			
+			if(keyValue[0] === key) {
+				return keyValue[1];
+			}
+		}
+	}
+}
+
+var getValueOfLastKey = function(key) {
+	if(key !== null || key.length > 0) {
+		var array = getArray();
+		
+		for(var i = array.length - 1; i >= 0; i--) {		
+			var keyValue = array[i].split('=');
+			
+			if(keyValue[0] === key) {
+				return keyValue[1];
+			}
+		}
+	}
+}
+
+var hasKey = function(key) {
+	if(isGetPresent()) {
+		var array = getArray();
+		
+		for(var i = 0; i < array.length; i++) {		
+			var keyValue = array[i].split('=');
+			
+			if(keyValue[0] === key) {
+				return true;
+			}
+		}
+		
+		return false;
+		
+	} else {
+		return false;
+	}
+}
