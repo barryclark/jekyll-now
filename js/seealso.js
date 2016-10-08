b0f4ad34dedf0080adc898a854b926032e761664
hendrixjoseph <hendrixjoseph@aol.com>
2016-10-08 01:09:10 -0400

    bug fix

--- js/seealso.js
+++ js/seealso.js
@@ -1,11 +1,10 @@
 function addSeeAlsos(posts) {
 	posts.sort(function(){return Math.random() - 0.5;});
-	  
+
 	for(var i = 0; i < Math.min(posts.length, 3); i++) {
 		post = posts[i];
-		
-		$("#see-also").append('<h3><a href="' + post.url + '>' + post.title + "</a></h3>");
+
+		$("#see-also").append('<h3><a href="' + post.url + '">' + post.title + "</a></h3>");
 		$("#see-also").append('<div class="excerpt">' + post.excerpt + "</div>");
-  } 
-	
+  }
 }
