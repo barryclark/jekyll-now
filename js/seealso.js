ab587697105aefb9795654e6608f2c0d473cef7d
hendrixjoseph <hendrixjoseph@aol.com>
2016-10-08 01:03:33 -0400

    oopsy

--- js/seealso.js
+++ js/seealso.js
@@ -3,10 +3,6 @@ function addSeeAlsos(posts) {
 	  
 	for(var i = 0; i < Math.min(posts.length, 3); i++) {
 		post = posts[i];
-	
-		var heading = ';
-		heading = heading + ;
-		heading = heading + post.title + "</a></h3>";
 		
 		$("#see-also").append('<h3><a href="' + post.url + '>' + post.title + "</a></h3>");
 		$("#see-also").append('<div class="excerpt">' + post.excerpt + "</div>");
