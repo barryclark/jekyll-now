8e0afd65465ae0154fca6655b9c8056c202ded11
Joseph Hendrix <hendrixjoseph@aol.com>
2016-10-10 15:29:14 -0400

    Update social.js

--- js/social.js
+++ js/social.js
@@ -1,8 +1,19 @@
 var social = {
-  shareToFacebook: function(url) {
-    FB.ui({
-        method: 'share',
-        href: url
-      }, function(response){});
-  }
+	facebook: {
+		share: function(url) {
+			FB.ui({
+				method: 'share',
+				href: url
+		  }	, function(response){});
+		},
+		like: function(url) {
+			FB.ui({
+				method: 'share_open_graph',
+				action_type: 'og.likes',
+				action_properties: JSON.stringify({
+					object: url,
+				})
+			}, function(response){});
+		}
+	}
 }
