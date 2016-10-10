12ebcaa1921cc61c902571b6024806028c68aaa2
Joseph Hendrix <hendrixjoseph@aol.com>
2016-10-10 16:38:22 -0400

    Update social.js

--- js/social.js
+++ js/social.js
@@ -1,19 +1,6 @@
 var social = {
-	appId: '197946313591875',
-	uid: null,
-	accessToken: null,
 	facebook: {
-		login: function() {
-			FB.getLoginStatus(function(response) {
-				if (response.status === 'connected') {
-    					this.uid = response.authResponse.userID;
-    					this.accessToken = response.authResponse.accessToken;
-  				} else if (response.status === 'not_authorized') {
-  				} else {
-    					// the user isn't logged in to Facebook.
-  				}	
-			});
-		},
+		accessToken: null,
 		share: function(url) {
 			FB.ui({
 				method: 'share',
@@ -30,7 +17,7 @@ var social = {
 			}, function(response){});
 		},
 		countLikes: function(url) {
-			FB.api('/' + url + '?access_token=' + this.appId, function(response) {
+			FB.api('/' + url + '?access_token=' + this.accessToken, function(response) {
   				console.log(response);
 			});	
 		}
