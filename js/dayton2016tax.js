53030853f647d9e8d1be2939264e4e6ad7eda420
Joseph Hendrix <hendrixjoseph@aol.com>
2016-10-18 16:07:55 -0400

    Update dayton2016tax.js

--- js/dayton2016tax.js
+++ js/dayton2016tax.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     var oldtax = income * 0.0225;
     var newtax = income * 0.025;
     var difference = income * 0.0025;
-    $("#dayton-tax-225").val(oldtax);
-    $("#dayton-tax-25").val(newtax);
-    $("#dayton-tax-025").val(difference);
+    $("#dayton-tax-225").val("$" + oldtax.toFixed(2));
+    $("#dayton-tax-25").val("$" + newtax.toFixed(2));
+    $("#dayton-tax-025").val("$" + difference.toFixed(2));
   });
 });
