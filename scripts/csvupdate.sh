a277eff325356ce64e252f15b9dd15856929c09f
Iltempe <teosoft80@gmail.com>
2016-11-26 23:49:36 +0100

    added links

--- scripts/csvupdate.sh
+++ scripts/csvupdate.sh
@@ -14,6 +14,7 @@ CSV_FABBISOGNI='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDein
 CSV_VITTIME='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=1445675693&single=true&output=csv'
 CSV_PRESS='https://docs.google.com/spreadsheets/d/1WL5BuoKQRM560VNctYOeDeineLeBwP7vtFlwltasASM/pub?gid=1534569987&single=true&output=csv'
 CSV_STORIE='https://docs.google.com/spreadsheets/d/e/2PACX-1vRfNyClIl9C7yvUJP7UAhoh9Al_pPKQeP5aKZ4lrnAjWtsWANelYI5qJOnJHBm2y1uj-Eqcx5Glc_Wl/pub?gid=512745056&single=true&output=csv'
+CSV_LINKS='https://docs.google.com/spreadsheets/d/e/2PACX-1vRfNyClIl9C7yvUJP7UAhoh9Al_pPKQeP5aKZ4lrnAjWtsWANelYI5qJOnJHBm2y1uj-Eqcx5Glc_Wl/pub?gid=2022353516&single=true&output=csv'
 #MD_vittime='http://blog.spaziogis.it/static/projs/terremotocentroitalia/vittime.md'
 
 wget -O _data/contatti.csv $CSV_CONTATTI
@@ -26,6 +27,8 @@ wget -O _data/fabbisogni.csv $CSV_FABBISOGNI
 wget -O _data/vittime.csv $CSV_VITTIME
 wget -O _data/press.csv $CSV_PRESS
 wget -O _data/storie.csv $CSV_STORIE
+wget -O _data/links.csv $CSV_LINKS
+
 
 #wget -O vittime.md $MD_vittime
 
