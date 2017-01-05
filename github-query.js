fd3ca9b55f78dd6a7493a74a04395c5c7387cdc2
JunShern <JunShern@users.noreply.github.com>
2017-01-05 17:13:36 +0000

    Added "header" class to card titles

--- github-query.js
+++ github-query.js
@@ -45,7 +45,7 @@ jQuery.fn.loadRepositories = function(username) {
                     //+ "<div style='text-align:center'><a style='' href='"+ repo.html_url + "''>" + repo.name + "</a></div>"
                     + "</div>"
                     + '<div class="extra content">'
-                        + "<a style='color:rgb(60,60,60)' href='"+ repo.html_url + "''>" + repo.name + "</a>"
+                        + "<a class='header' style='color:rgb(60,60,60)' href='"+ repo.html_url + "''>" + repo.name + "</a>"
                     + "</div>"
                     + '<div class="extra content">'
                         + "<div class='description'><p>" + repo.description + "</p></div>"
