a59d4d19bc7c4a107fae78c31e48756ee7e74822
Hrayr <epimoz@yahoo.com>
2017-02-05 10:50:52 -0800

    updating a few sections

--- _posts/Euler/Euler_1.py
+++
@@ -1,9 +0,0 @@
-#Find the sum of all the multiples of 3 or 5 below 1000
-
-def Euler1(a):
-    total = 0
-    x = 1
-    while x < 1000 and (x%3 == 0 or (x%5 == 0 and x%15 != 0)):
-        total += x
-        x += 1
-    print(total)
