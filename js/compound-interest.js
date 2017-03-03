7952f593169e43b0fa02005a53581f6209a51f1c
Joseph Hendrix <hendrixjoseph@aol.com>
2017-03-03 12:45:20 -0500

    Update compound-interest.js

--- js/compound-interest.js
+++ js/compound-interest.js
@@ -26,7 +26,12 @@ $(document).ready(function() {
     balance.push(previous);
     interest.push(previous * rate / period);
     deposits.push(invest);
-    time.push((p / period).toString());
+	  
+    if(p % period = 0) {
+       time.push((p / period).toString());
+    } else {
+       time.push('');
+    }
     
     previous = previous + previous * rate / period + invest;
   }
@@ -37,7 +42,8 @@ $(document).ready(function() {
     data: {
         columns: [time, balance, interest, deposits],
         type: 'bar',
-    	groups: [['balance', 'interest', 'deposit']]
+    	groups: [['balance', 'interest', 'deposit']],
+	order: null
     },
     axis: {
     	x: {type: 'category',
