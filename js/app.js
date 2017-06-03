86f061df1c85faede117fc89516a87aa72e226ac
Matt <matthewmacintyre@gmail.com>
2017-06-03 13:35:10 -0300

    Finalized IG sidebar widget
    Updated with IG sidebar widget and reduced number of tweets shown. Good for now.

--- js/app.js
+++ js/app.js
@@ -7,12 +7,11 @@ $(function(){
 });
 var feed = new Instafeed({
   get: 'user',
-  resolution: 'low_resolution',
+  resolution: 'standard_resolution',
   userId: '23503',
   clientId: '9986ee88c1954d768989ac497ddb5c7a',
   accessToken: '23503.9986ee8.9342a7e129604e42a65755bb584443bd',
   template: '<a class="slide" href="{{link}}"><span>{{caption}} // <i class="fa fa-heart" aria-hidden="true"></i> {{likes}}</span><img src="{{image}}" /></a>'
-  // template: '<img src="{{image}}" />'
 });
 feed.run();
 
@@ -25,9 +24,4 @@ jQuery(window).on('load', function() {
     pager: false,
     infiniteLoop: false
   });
-  // jQuery('.owl-carousel').owlCarousel({
-  //   loop:false,
-  //   items: 1,
-  //   nav: true
-  // });
 });
