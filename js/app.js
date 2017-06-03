f4266ec5d8ba3cf938692bdb5512b4149b97f6ec
thinkontheclock <matthewmacintyre@gmail.com>
2017-06-03 13:13:34 -0300

    Adding some libs
    Adding some IG / Twitter libs, working on a better / more dynamic sidebar.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,29 @@ $(function(){
     $(this).toggleClass('active');
   });
 });
+var feed = new Instafeed({
+  get: 'user',
+  resolution: 'low_resolution',
+  userId: '23503',
+  clientId: '9986ee88c1954d768989ac497ddb5c7a',
+  accessToken: '23503.9986ee8.9342a7e129604e42a65755bb584443bd',
+  template: '<a class="slide" href="{{link}}"><span>{{caption}} // <i class="fa fa-heart" aria-hidden="true"></i> {{likes}}</span><img src="{{image}}" /></a>'
+  // template: '<img src="{{image}}" />'
+});
+feed.run();
+
+jQuery(window).on('load', function() {
+  $('#instafeed').bxSlider({
+    minSlides: 1,
+    maxSlides: 1,
+    mode: 'vertical',
+    slideMargin: 0,
+    pager: false,
+    infiniteLoop: false
+  });
+  // jQuery('.owl-carousel').owlCarousel({
+  //   loop:false,
+  //   items: 1,
+  //   nav: true
+  // });
+});
