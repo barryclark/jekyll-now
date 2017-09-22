b81612e75d92370fb105eebce97138bab8c9fcd2
sternj20 <jessesternmusic@gmail.com>
2017-09-22 14:51:10 -0700

    customized view of projects for portfolio work. added text that gets displayed on hover that tells you more about the project. add images for the projects

--- assets/js/scrolling-nav.js
+++ assets/js/scrolling-nav.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+    $('.gallery-item').hover( function() {
+        $(this).find('.img-title').fadeIn(300);
+    }, function() {
+        $(this).find('.img-title').fadeOut(100);
+    });
+    
     //scrollspy
     //smooth scrolling
     $("#toTop").hide();
