5c030ab82032a1f2639e4d51788d0f4664c0ef69
Matteo Fortini <matteo.fortini@gmail.com>
2017-11-06 08:51:53 +0100

    Prova mapillary photostory

---
+++ mapillary/photostory001.js
@@ -0,0 +1,7 @@
+var project_title = 'Terremoto Centro Italia';
+var project_subtitle = 'Le immagini di più di un anno';
+var images = ['27FW98irrFbbWI_CGgCebQ','bCd7O6PhM8BABuVB-q340A','1d70jY76gxQuKvQcQUzErw','8oI1aWlduJT7WcH0tdLV-g','bZXIpYhlkCOIbL4Oil3yhQ'];
+var titles = ['L'Aquila 1','L'Aquila 2','L'Aquila 3','Arquata del Tronto 1','Arquata del Tronto 2'];
+var content = ['','','','',''];
+var xcoords = ['','','','',''];
+var ycoords = ['','','','',''];
---
+++ mapillary/photostory001.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: Mapillary Photostory 001
+permalink: /mapillary/photostory001
+---
+
+<div class="row"><div class="col-md-12"> 
+<iframe width="100%" src="https://mapillary-hacks.github.io/photostories/?src=/mapillary/photostory001.js"></iframe>
+</div>
+</div>
