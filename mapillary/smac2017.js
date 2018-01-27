469b7e982d435cf68bdc8c5e642fe04555b8898f
Matteo Fortini <matteo.fortini@gmail.com>
2018-01-27 16:29:13 +0100

    Aggiunta pagina mapillary SMAC2017

---
+++ mapillary/smac2017.js
@@ -0,0 +1,23 @@
+var project_title = 'SMAC';
+var project_subtitle = 'Scuola di Monitoraggio e Azione Civica - Arquata del Tronto';
+var images = [
+'N_Hvr3T3OpHOH1kD1ATJNg',
+'o6TbbcEQ-M5cgRpYVLp2QQ',
+'NNjUtpVAMx4tYM_DeRtAdQ',
+'dk8RsBVQI-q36F7uj-l-ww',
+'jVyc1lyGY9ReTWvNAkUarA',
+'4AmW70EYkoPSFT39YT5vIQ'];
+var titles = ['Arquata del Tronto',
+'Arquata del Tronto',
+'SMAC - Albergo Regina Giovanna',
+'SMAC - Oratorio di Santi Pietro e Paolo',
+'Arquata del Tronto',
+'Le SAE'];
+var content = ['La zona rossa di Arquata del Tronto',
+'La zona rossa di Arquata del Tronto',
+'A distanza di un anno, Albergo Regina Giovanna',
+'A distanza di un anno, la casa dell\'Oratorio dei Santi Pietro e Paolo',
+'',
+'Arquata del Tronto, le SAE];
+var xcoords = ['','','','',''];
+var ycoords = ['','','','',''];
