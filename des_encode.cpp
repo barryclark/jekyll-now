8a8478ed10b028b580902f08390e8fb2c3dc1c25
Joseph Hendrix <hendrixjoseph@aol.com>
2018-02-26 22:29:22 -0500

    Add files via upload

--- des_encode.cpp
+++ des_encode.cpp
@@ -20,20 +20,20 @@ void des_encryption_8(unsigned char *input, unsigned char *key, unsigned char *o
 	    desEncryptor.ProcessAndXorBlock(input,xorBlock,output);
 }
 
-void des_encryption(char *plaintext, char *key, char *ciphertext) {
-	char *subtext;
-	char *subcipher;
+void des_encryption(unsigned char *plaintext, unsigned char *key, unsigned char *ciphertext) {
+	unsigned char *subtext;
+	unsigned char *subcipher;
 	
 	for(int i=0; i < sizeof plaintext; i=i+8) {
 		int start = i;
 		int end = start + 8;
 		copy(plaintext + start, plaintext + end, subtext);
-		des_encryption_8(subtext, key, subcipher)
-		copy(subcipher, subcipher + 8, ciphertext + start)
+		des_encryption_8(subtext, key, subcipher);
+		copy(subcipher, subcipher + 8, ciphertext + start);
 	}
 }
 
-void read_key(char *keystring, byte key[]) {
+void read_key(unsigned char *keystring, unsigned char *key) {
 	memset(key,0,DES::DEFAULT_KEYLENGTH);
 		
 	for(int i=0;i<DES::DEFAULT_KEYLENGTH;i++) {
@@ -50,9 +50,9 @@ int main(int argc, char * argv[]) {
 	ifstream infile;
 	ofstream outfile;
 	streampos size;
-	char *plaintext;
-	char *ciphertext;
-	byte key[DES::DEFAULT_KEYLENGTH];
+	unsigned char *plaintext;
+	unsigned char *ciphertext;
+	unsigned char *key;
 
 	if(argc!=4)	{
 		cout<<"usage:des_encode infile outfile key"<<endl;
@@ -61,9 +61,8 @@ int main(int argc, char * argv[]) {
 		outfile.open(argv[2], ios::out | ios::binary);
 		
 		if (infile.is_open()) {
-
 			size = infile.tellg();
-			plaintext = new char [size];
+			plaintext = new unsigned char[size];
 			infile.seekg(0, ios::beg);
 			infile.read(plaintext, size);
 			
