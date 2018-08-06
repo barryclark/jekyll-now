605389ca303f9400e825e394f1725517a47484ee
David Delabassee <david.delabassee@gmail.com>
2018-08-06 08:23:55 +0200

    Create 2014-12-4-EE-7-Tutorial.m

---
+++ _posts/2014-12-4-EE-7-Tutorial.m
@@ -0,0 +1,18 @@
+---
+layout: post
+title: Interceptors in Java EE
+excerpt: The Java EE 7 Documentation page has been recently refreshed ...
+---
+
+The [Java EE 7 Documentation](http://docs.oracle.com/javaee/7/) page has been recently refreshed with an improved and cleaner layout. It should now be easier to locate, browse and read the Java EE API documentation, the Java EE SDK documentation and the Java EE Tutorials.
+
+It is sometime useful to remind about the availability of useful resources such the _[Java EE Tutorial](https://docs.oracle.com/javaee/7/tutorial/index.html)_… even more when those resources a free!  The Java EE Tutorial is one of those gems that is sometime overlooked.  It covers Java EE 7 and is an invaluable set of resources for anyone wanting to learn Java EE but also for people already familiar with Java EE as it also covers some of the less known part of the Java EE platform such as JCA.
+
+For new comers, there is _[Your First Cup: An Introduction to the Java EE Platform](https://docs.oracle.com/javaee/7/firstcup/index.html)_, a crisp introduction to the Java EE platform.
+
+The _[Java EE 7 Tutorial](https://docs.oracle.com/javaee/7/tutorial/index.html)_ is available for free (online and PDF format).  In addition, the Java EE Tutorial is now also available in ePub and Mobi formats (hint: check the top right corner) so you can download and read it on your preferred eBook reader (tablets, iBook, Kindle, etc.).  Just check the top right corner.  And again, those great resources are free!
+
+Finally if you are more old school, the _Java EE 7 Tutorial_ is also available, for a fee, as a [dead-tree version](http://www.amazon.com/Java-EE-Tutorial-5th/dp/0321994922/). 
+
+
+*Originaly posted on [The Aquarium](https://blogs.oracle.com/theaquarium/the-java-7-ee-tutorial-free-ebook) blog.*
