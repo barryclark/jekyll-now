ad6037f1fd820ad9b3e28e197635b634382c2c5a
Isard Miralles Torrecillas <isardmt@gmail.com>
2019-03-25 21:09:36 +0100

    Add files via upload

--- hipertextos/js/fases.js
+++ hipertextos/js/fases.js
@@ -1,10 +1,10 @@
 var thehours = new Date().getHours();
 
 	var themessage;
-	var night = ('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-night.gif">');
-	var morning = ('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-morning.gif">');
-	var afternoon = ('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-afternoon.gif">');
-	var evening = ('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-evening.gif">');
+	var night = ('<img id="sun-phase" src="/hipertextos/cursors/sun-night.gif">');
+	var morning = ('<img id="sun-phase" src="/hipertextos/cursors/sun-morning.gif">');
+	var afternoon = ('<img id="sun-phase" src="/hipertextos/cursors/sun-afternoon.gif">');
+	var evening = ('<img id="sun-phase" src="/hipertextos/cursors/sun-evening.gif">');
 
 	if (thehours >= 0 && thehours < 6) {
 		themessage = night; 
@@ -32,18 +32,18 @@ $('#sun-phase').html(themessage);
 
 $(function() {
    $('.th-aurora').click(function() {
-   		$('#sun-phase').html('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-morning.gif">');
+   		$('#sun-phase').html('<img id="sun-phase" src="/hipertextos/cursors/sun-morning.gif">');
    });
 
    $('.th-meridianus').click(function() {
-   		$('#sun-phase').html('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-afternoon.gif">');
+   		$('#sun-phase').html('<img id="sun-phase" src="/hipertextos/cursors/sun-afternoon.gif">');
    });
 
    $('.th-vesperum').click(function() {
-   		$('#sun-phase').html('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-evening.gif">');
+   		$('#sun-phase').html('<img id="sun-phase" src="/hipertextos/cursors/sun-evening.gif">');
    });
 
    $('.th-nox').click(function() {
-   		$('#sun-phase').html('<img id="sun-phase" src="https://acozar.github.io/src/img/sun-night.gif">');
+   		$('#sun-phase').html('<img id="sun-phase" src="/hipertextos/cursors/sun-night.gif">');
    });
 });
