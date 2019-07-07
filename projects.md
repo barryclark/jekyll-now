3e5734b220f7817add80d24d4201ff19dd5ca3b3
Renan Liberato <renan.liberato@garnon.me>
2019-07-07 11:53:30 -0300

    Added simple sketch

--- projects.md
+++ projects.md
@@ -13,3 +13,9 @@ Here you will find some personal/professional projects that I have worked or am 
 Simple speed reading web app made with React/Redux that allows you to read a text in as little time as you can.
 
 <a href="/projects/fastreader">Check it out!</a>
+
+#### Simple sketch
+
+A web UI for creating simple diagrams.
+
+<a href="/projects/simplesketch">Check it out!</a>
---
+++ projects/simplesketch/precache-manifest.5253da1c9b8c9d766ba4e785e4358bcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f055e28b44a5942a1d372fc18478de2d",
+    "url": "/projects/simplesketch/index.html"
+  },
+  {
+    "revision": "de61513441f12364e999",
+    "url": "/projects/simplesketch/static/css/main.c63dc2c0.chunk.css"
+  },
+  {
+    "revision": "b0303a53887b06e87051",
+    "url": "/projects/simplesketch/static/js/2.c9e41c47.chunk.js"
+  },
+  {
+    "revision": "de61513441f12364e999",
+    "url": "/projects/simplesketch/static/js/main.28c90c50.chunk.js"
+  },
+  {
+    "revision": "0061963a7eaf228141ef",
+    "url": "/projects/simplesketch/static/js/runtime~main.ead2077c.js"
+  }
+]);
