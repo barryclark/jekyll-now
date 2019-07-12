ce45be152907d80cccd64789328eed7ba6711159
hendrixjoseph <hendixjoseph@aol.com>
2019-07-12 16:27:34 -0400

    new post

---
+++ _posts/2019-07-12-rethinking-the-flag-of-huber-heights-ohio.m
@@ -0,0 +1,53 @@
+---
+layout: post
+title: Rethinking the Flag of Huber Heights, Ohio
+tags: [vexillology]
+keywords: [flag of huber heights, flag, flags, huber heights, huber heights flag]
+image: /images/flags/huber-heights-gen.png
+---
+
+I recently watched the following video on [why city flags may be the worst-designed thing you've never noticed](https://www.youtube.com/watch?v=pnv5iKB2hl4) and began thinking about local municipality flags:
+
+{% include youtube.html param="pnv5iKB2hl4" %}
+
+Huber Heights does have a flag, but the only image I could find from the [City Flag and City Seal ](https://www.hhoh.org/362/City-Flag-City-Seal) on the city webpage is rather low resolution:
+
+![Low-resolution (250px × 149px) JPEG of the flag of Huber Heights, Ohio](/images/flags/huber-heights/huber-heights.png)
+*Low-resolution (250px × 149px) JPEG of the flag of Huber Heights, Ohio*
+
+I wanted to update it based on ideas in the video, but first I made [an SVG version of the Huber Heights flag](/images/flags/huber-heights/huber-heights.svg). The following is an exported PNG of that SVG:
+
+![Better rendering of the flag of Huber Heights, Ohio](/images/flags/huber-heights/huber-heights-gen.png)
+*Better rendering of the flag of Huber Heights, Ohio*
+
+Now onto some of the core concepts from the video. Namely:
+
+1. Keep it simple.
+2. Use meaningful symbolism.
+3. Use 2-3 basic colors.
+4. No letters or seals.
+5. Be distinctive or be related.
+
+The first thing I did was remove the seal in the center and the text:
+
+![The Flag of Huber Heights, Ohio with the text and seal removed.](/images/flags/huber-heights/my-huber-heights-1.png)
+*The Flag of Huber Heights, Ohio with the text and seal removed.*
+
+I felt the stars had no symbolism or purpose, as well as just looking awkward, so I removed those as well:
+
+![The Flag of Huber Heights, Ohio with the stars removed.](/images/flags/huber-heights/my-huber-heights-2.png)
+*The Flag of Huber Heights, Ohio with the stars removed.*
+
+Next, I felt that the wide border around the two H's (aches?) made the flag too complex, so I removed them (or made them the same shade of green as the H's):
+
+![The Flag of Huber Heights, Ohio with no white border around the H's.](/images/flags/huber-heights/my-huber-heights-3.png)
+*The Flag of Huber Heights, Ohio with no white border around the H's.*
+
+I think that looks fine, but I also wanted to play with the colors a bit. I changed the colors to match [the hex colors of the Flag of the United States based on the discussion on Wikimedia](https://commons.wikimedia.org/wiki/File_talk:Flag_of_the_United_States.svg#Color_specifications_2):
+
+![The Flag of Huber Heights, Ohio in Red, White, and Blue](/images/flags/huber-heights/my-huber-heights-4.png)
+*The Flag of Huber Heights, Ohio in Red, White, and Blue*
+
+These hex colors are <span style="background: #BF0A30;color: white;padding: 0 0.5em;">#BF0A30 (red)</span>, #FFFFFF (white), and <span style="background: #BF0A30;color: white;padding: 0 0.5em;">#002868 (blue)</span>.
+
+I've had a few thoughts on other flags, such as Montgomery County Ohio, Greene County Ohio, and Toastmasters, but that's a post for another day.
