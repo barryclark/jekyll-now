2cf252916cf24ff6b7385d3b07b0e91ceb8369d1
parra <parrazam@users.noreply.github.com>
2019-08-25 18:05:08 +0200

    Create publishMessageToRaspiBot.sh

---
+++ scripts/publishMessageToRaspiBot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+  echo "\nSe requiere un mensaje para enviar. Ejemplo de uso: $0 \"Mensaje de prueba\""
+  exit 1
+fi
+
+CHAT_ID=<CHAT_ID>
+API_KEY=<BOT_TOKEN>
+
+MESSAGE=${1// /+}
+URL="https://api.telegram.org/bot$API_KEY/sendMessage?chat_id=$CHAT_ID&text=$MESSAGE&parse_mode=Markdown"
+
+curl -i -H "Accept: application/json" -X GET -g $URL
+
+if [[ $? -eq 0 ]]
+then
+  echo "\nMensaje $1 publicado."
+else
+  echo "\nError al publicar el mensaje. Consulta el log para más detalle."
+  exit 2
+fi
+
+exit 0
