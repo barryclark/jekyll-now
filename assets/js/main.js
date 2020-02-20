d204a6e7fce99970bc49ac5181d6cf73e4b9de98
Francisco Sanchez <francisco.sanchez@salesforce.com>
2020-02-20 08:14:47 -0800

    Create subscription popup and add ordering names post

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,14 @@
 // Copyright © 2019 Sanchez Parra Labs. All rights reserved.
 //
 (function() {
-
+  var popup = document.getElementsByClassName("popup");
+  popup[0].addEventListener('click', function(e) {
+    if (e.target == '_blank') {
+      return true;
+    } else {
+      window.open(e.target.href, 'popupwindow', 'scrollbars=yes,width=600,height=600');
+      e.preventDefault();
+      return false;
+    }
+  }, false);
 }).call(this);
