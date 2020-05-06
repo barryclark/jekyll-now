2a13f2ef1d50ffae605cc82db2f35be86692a013
Rocky <51120563+Rockycuxin@users.noreply.github.com>
2020-05-06 19:38:14 +0800

    Create style.js

---
+++ doudoudiannao.com/js/style.js
@@ -0,0 +1,13 @@
+name = prompt('你是谁？')
+docment.white('欢迎'+name)
+password = prompt('密码是？游客不用填')
+pswd = "159357rrrocky521"
+pswd1 = "admin"
+if password == pswd{
+  docment.white('身份id：管理员Rocky')
+} elif password == pswd1{
+  docment.white('登陆身份：管理员 店里人')
+else{
+  alert('你好！')
+  docment.white('登陆人：游客')
+}
