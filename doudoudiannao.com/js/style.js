822d6dedd49f8b3b3db4564f0e79813d14657805
Rocky <51120563+Rockycuxin@users.noreply.github.com>
2020-05-07 13:36:12 +0800

    Update style.js

--- doudoudiannao.com/js/style.js
+++ doudoudiannao.com/js/style.js
@@ -1,13 +1,22 @@
-name = prompt('你是谁？')
-docment.white('欢迎'+name)
-password = prompt('密码是？游客不用填')
-pswd = "159357rrrocky521"
-pswd1 = "admin"
-if password == pswd{
-  docment.white('身份id：管理员Rocky')
-} elif password == pswd1{
-  docment.white('登陆身份：管理员 店里人')
-else{
-  alert('你好！')
-  docment.white('登陆人：游客')
+function Login()
+{
+  name = prompt('你是谁？')
+  docment.white('欢迎'+name)
+  dengluzhuangtai = 'zhengzaidenglu'
+  password = prompt('密码是？游客不用填')
+  pswd = "159357rrrocky521"
+  pswd1 = "admin"
+  if (password == pswd){
+    dengluzhuangtai = 'superuser-rocky'
+    docment.white('身份id：管理员Rocky,别忘去hxcpp8.com')
+  } elif (password == pswd1){
+    dengluzhuangtai = 'dianliren'
+    docment.white('登陆身份：管理员 店里人')
+  else{
+    alert('你好！')
+    docment.white('登陆人：游客')
+    dengluzhuangtai = 'youke'
+  }
+
 }
+Login()
