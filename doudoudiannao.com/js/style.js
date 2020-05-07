e946fb51568e550f0671ff51b9354772af5aed4b
Rocky <51120563+Rockycuxin@users.noreply.github.com>
2020-05-07 14:54:22 +0800

    Update style.js
    dfdgf

--- doudoudiannao.com/js/style.js
+++ doudoudiannao.com/js/style.js
@@ -1,22 +1,23 @@
 function Login()
 {
+  function pass()
+  {
+    a = 1
+  }
   name = prompt('你是谁？')
-  docment.white('欢迎'+name)
+  docment.write('欢迎'+name)
   dengluzhuangtai = 'zhengzaidenglu'
   password = prompt('密码是？游客不用填')
   pswd = "159357rrrocky521"
   pswd1 = "admin"
   if (password == pswd){
     dengluzhuangtai = 'superuser-rocky'
-    docment.white('身份id：管理员Rocky,别忘去hxcpp8.com')
+    docment.write('你好啊开发者 Rocky')
   } elif (password == pswd1){
-    dengluzhuangtai = 'dianliren'
-    docment.white('登陆身份：管理员 店里人')
-  else{
-    alert('你好！')
-    docment.white('登陆人：游客')
-    dengluzhuangtai = 'youke'
+    dengluzhuangtai = 'suprouser-dad'
+    docment.write()
+  } else{
+    pass()
   }
-
 }
 Login()
