acf27cc1723a3e36ed13900cbcee4d4c36341871
Dactrtr <sebastian.zuniga@elun.cl>
2020-05-14 10:42:04 -0400

    more micro updates

--- how/crazycode.js
+++ how/crazycode.js
@@ -1,4 +1,6 @@
 let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(".nav-links li a");
+let headtitle = document.querySelector("a.blog");
 
 window.addEventListener('mousemove',cursor);
 
@@ -6,3 +8,21 @@ function cursor(e){
   mouseCursor.style.top = e.pageY +"px";
   mouseCursor.style.left = e.pageX +"px";
 }
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseover',() =>{
+    mouseCursor.classList.add('link-mouse')
+    link.classList.add("hover-link");
+  });
+  link.addEventListener('mouseleave',() =>{
+    mouseCursor.classList.remove('link-mouse')
+    link.classList.remove("hover-link");
+  });
+});
+
+headtitle.addEventListener('mouseover',() =>{
+  headtitle.classList.add('glitch');
+});
+headtitle.addEventListener('mouseleave',() =>{
+  headtitle.classList.remove('glitch');
+});
