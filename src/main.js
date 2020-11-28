973df612fc07cad3dacff6033404c8d7733b8baf
ixap2i <ayamashita124@gmail.com>
2020-11-29 00:30:11 +0900

    tailwindcssの読み込み修正とテーマファイルを作成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueTailwind from 'vue-tailwind'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import 'material-design-icons/iconfont/material-icons.css'
+import "tailwindcss/tailwind.css"
 
 import settings from './theme.js'
 
--- src/theme.js
+++ src/theme.js
@@ -9,12 +9,14 @@ const TInput = {
 }
 
 const TButton = {
-  classes: 'rounded-lg border block inline-flex items-center justify-center',
+  fixedClasses: 'focus:outline-none focus:shadow-outline inline-flex items-center transition ease-in-out duration-150',
+  classes: 'text-white bg-blue-600 hover:bg-blue-500 focus:border-blue-700 active:bg-blue-700 text-sm font-medium border border-transparent px-3 py-2 rounded-md',
   variants: {
-    secondary: 'rounded-lg border block inline-flex items-center justify-center bg-purple-500 border-purple-500 hover:bg-purple-600 hover:border-purple-600',
-  },
-  // Optional fixedClasses
-  // fixedClasses: 'transform ease-in-out duration-100',
+    error: 'text-white bg-red-600 hover:bg-red-500 focus:border-red-700  active:bg-red-700 text-sm font-medium border border-transparent px-3 py-2 rounded-md',
+    success: 'text-white bg-green-600 hover:bg-green-500 focus:border-green-700 active:bg-green-700 text-sm font-medium border border-transparent px-3 py-2 rounded-md',
+    funny: 'text-white bg-orange-600 hover:bg-orange-500 focus:border-orange-700 active:bg-orange-700 text-sm font-medium uppercase border-orange-200 px-4 py-2 border-2 rounded-full shadow',
+    link: 'underline text-orange-500 px-3 py-2 hover:bg-orange-100 rounded'
+  }
 }
 
 const settings = {
