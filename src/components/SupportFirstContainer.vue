82a71eecbf9e774af055e69418835b55e72f7de8
Yukina Ogasawara <s1f101900723@iniad.org>
2021-01-02 22:57:01 +0900

    ご支援・ご協力ページをcomponentsに追加

---
+++ src/components/SupportFirstContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="support_first-container relative bg-white overflow-hidden row">
+    <main class='pt-64'>
+      <div class="container support_first-box">
+        <t-tag variant="heading" class='text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9'>
+          ご支援・ご協力のお願い
+        </t-tag>
+        <t-tag tag-name="p" variant="body" class='pt-5 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-xl sm:leading-9'>
+            ご支援・ご協力をお願い致します。
+            ご支援・ご協力をお願い致します。
+            ご支援・ご協力をお願い致します。
+            ご支援・ご協力をお願い致します。
+        </t-tag>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+main { width: 100%; }
+.support_first-box {
+  background: #fff;
+  opacity: 0.8;
+  width: 640px;
+}
+.support_first-container {
+  background: url('../assets/person-using-macbook-pro-on-person-s-lap-1181298.jpg') no-repeat right bottom;
+  background-size: contain;
+  margin: 0 auto;
+  height: 640px;
+  width: 100%;
+}
+</style>
