3714e54991b1f6c6a522271216a4588961cbb428
ixap2i <ayamashita124@gmail.com>
2021-03-01 22:08:52 +0900

    ブログエリアのスタイルの修正。

--- src/components/top/FifthContainer.vue
+++ src/components/top/FifthContainer.vue
@@ -8,42 +8,47 @@
         <div class="col s12 m3">
           <div class="card">
             <div class="card-image">
-              <img class="carousel-img" src="../../assets/card_2.png">
+              <img class="carousel-img" src="../../assets/blogs/blog-i-1.png">
             </div>
             <div class="card-content">
-              <t-tag variant="heading" class='pb-5 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9 text-center'>
-                活動の様子
+              <t-tag variant="heading" class='blog-heading truncate text-xl pb-5 font-semibold tracking-tight font-display text-gray-900 text-center'>
+                第12回 子どものためのプログラミング道場オンライン - CoderDojo立川（調布と合同開催）
               </t-tag>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+              <p class='blog-desc'>
+                今週のCoderDojo立川は、CoderDojo調布との合同開催。oViceというツールを使って、オンラインだけど実際にワークショップ会場に集まったようなバーチャル体験をしました。
+              </p>
             </div>
           </div>
         </div>
         <div class="col s12 m3">
           <div class="card">
             <div class="card-image">
-              <img class="carousel-img" src="https://lh3.googleusercontent.com/lDPrGvYT1exvQLOKIhcZvgAmIV3OUhaFHgNvUlEluJouA7AyeUuqr8Zf7Kv2KxROM6dWzuLLGharLA6piMPuYzCUaRu_iVVe5_9KedXLWo-0J6PsYzLTnp6hwoWOgDAuUDgXxNeOXA=w2400">
+              <img class="carousel-img" src="../../assets/blogs/blog-i-2.png">
             </div>
             <div class="card-content">
-              <t-tag variant="heading" class='pb-5 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9 text-center'>
-                活動の様子
+              <t-tag variant="heading" class='blog-heading truncate text-xl pb-5 font-semibold tracking-tight font-display text-gray-900 text-center'>
+                冬休み特別編 子どものためのプログラミング道場 - CoderDojo立川（DojoCon2020共催）
               </t-tag>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+              <p class='blog-desc'>
+                CoderDojo立川（自宅PCのScratchから、東京のEV3をリモート操作）
+                ▼2020年最後のイベント（12/27）は、全国規模のDojoCon2020にワークショップを共催しました。
+              </p>
             </div>
           </div>
         </div>
         <div class="col s12 m3">
           <div class="card">
             <div class="card-image">
-              <img class="carousel-img" src="../../assets/card_3.jpg">
+              <img class="carousel-img" src="../../assets/blogs/blog-i-3.png">
             </div>
             <div class="card-content">
-              <t-tag variant="heading" class='pb-5 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9 text-center'>
-                活動の様子
+              <t-tag variant="heading" class='blog-heading truncate text-xl pb-5 font-semibold tracking-tight font-display text-gray-900 text-center'>
+                第11回 子どものためのプログラミング道場 - CoderDojo立川（調布と合同開催）
               </t-tag>
-              <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
+              <p class='blog-desc'>
+                CoderDojo立川（調布と合同開催）
+                11/29は、調布チームによる会場開催と立川チームによるオンラインの合同開催となりました。調布チームは、ニンジャがそれぞれ自分の好きな作品の制作に取り組み、立川チームは、みんなでアイディアを出し合ってプログラムを制作する「モブプロ」（群衆によるプログラミングという意味）を実施。
+              </p>
             </div>
           </div>
         </div>
@@ -56,6 +61,18 @@
 main { width: 100%; }
 .row .col.m3 {
   margin-left: initial;
+}
+.blog-heading {
+  height: 1.6rem;
+  width: 16rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.blog-desc {
+  height: 7.2rem;
+  width: 16rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 .hero-box {
   opacity: 0.8;
