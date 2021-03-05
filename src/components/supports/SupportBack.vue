1e4a87bf547b666f0503d99d1b29364caa07437b
Yukina Ogasawara <s1f101900723@iniad.org>
2021-03-05 14:24:12 +0900

    slider 背景に設定

--- src/components/supports/SupportBack.vue
+++ src/components/supports/SupportBack.vue
@@ -1,35 +1,43 @@
 <template id="app">
-  <div class="support_second-container relative bg-white overflow-hidden row">
+  <div class="support_back-container relative bg-white overflow-hidden row">
     <main class='lg:container flex items-center'>
+          <div class="container support_back-box">
+        <t-tag variant="heading" class='text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9'>
+          こんにちは
+        </t-tag>
+        <t-tag tag-name="p" variant="body" class='pt-5 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-xl sm:leading-9'>
+          こんにちはー
+        </t-tag>
+      </div>
     <hooper :settings="hooperSettings">
       <slide>
         <div class="images img1">
           
-          <video src="../../assets/loop_1/1.mp4" loop controls></video>
+          <video src="../../assets/loop_1/1.mp4" loop autoplay controls></video>
         </div>
       </slide>
       <slide>          
         <div class="images img2">
           
-          <video src="../../assets/loop_1/2.mp4" loop controls></video>
+          <video src="../../assets/loop_1/2.mp4" loop autoplay controls></video>
         </div> 
       </slide>
       <slide>          
         <div class="images img3">
           
-          <video src="../../assets/loop_1/1.mp4" loop controls></video>
+          <video src="../../assets/loop_1/1.mp4" loop autoplay controls></video>
         </div>       
       </slide>
       <slide>  
         <div class="images img4">
           
-          <video src="../../assets/loop_1/1.mp4" loop controls></video>
+          <video src="../../assets/loop_1/1.mp4" loop autoplay controls></video>
         </div> 
       </slide>
       <slide>     
         <div class="images img5">
           
-          <video src="../../assets/loop_1/1.mp4" loop controls></video>
+          <video src="../../assets/loop_1/1.mp4" loop autoplay controls></video>
         </div>
       </slide>
     </hooper>
@@ -38,12 +46,21 @@
 </template>
 
 <style>
-main { width: 100%; }
-.support_second-box {
+main {
+  width: 100%; 
+  position: absolute;
+}
+.support_back-box {
+  position: absolute;
+  z-index: 2;
   opacity: 0.8;
   width: 640px;
+  height: 640px;
+  background: #fff;
 }
-.support_second-container {
+.support_back-container {
+  position: absolute;
+  z-index: 1;
   background: #FFF25A;
   display: flex;
   justify-content: center;
