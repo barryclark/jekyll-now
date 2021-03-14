7bf67172560645e62a33b6989dcaacb20b6d41aa
Melissa Reynolds <melissa.reynolds@princeton.edu>
2021-03-14 16:30:43 -0400

    Building sidebar navigation and adding an "About" preface

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(document) {
+  var toggle = document.querySelector('.sidebar-toggle');
+  var sidebar = document.querySelector('#sidebar');
+  var checkbox = document.querySelector('#sidebar-checkbox');
+
+  document.addEventListener('click', function(e) {
+    var target = e.target;
+
+    if(!checkbox.checked ||
+       sidebar.contains(target) ||
+       (target === checkbox || target === toggle)) return;
+
+    checkbox.checked = false;
+  }, false);
+})(document);
