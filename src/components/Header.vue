168789e0ed8c157618ff32e7b8f3146c176255f1
Yukina Ogasawara <s1f101900723@iniad.org>
2021-05-17 12:35:45 +0900

    type位置

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,9 +26,9 @@
             </router-link>
           </li>
           <li>
-            <router-link class="router-link" to="/members">
+            <a class="router-link" href="https://activo.jp/articles/72556?via=user105899">
               メンバー募集
-            </router-link>
+            </a>
           </li>
           <li>
             <a class="router-link" href="https://coderdojotachikawa.tamaliver.jp/">
