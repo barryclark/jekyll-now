f81fbbc6a0d302ab7d1dd47927e82c25437da6c7
IchiroKano <ichiro.kano2@asahi.email.ne.jp>
2021-06-06 22:46:05 +0900

    revise iframe

--- src/components/staffs/Members.vue
+++ src/components/staffs/Members.vue
@@ -28,16 +28,11 @@
   </div>
 </div>
 </div> 
-</div>
-<div class="">
-  <iframe src="https://member-cd.web.app/?dojo=tachikawa" width="100%" height="100" id="member-frame" frameborder="0" scrolling="yes" allowfullscreen></iframe>
-  <script>
-    window.addEventListener('message', function(e) {
-      if(e.origin=="https://member-cd.web.app"){
-        document.getElementById('member-frame').height = e.data + 120;
-      }
-    }, false);
-  </script>
+<br/>
+<br/>
+<div class="w-screen h-screen mx-auto bg-white shadow-md overflow-hidden md:max-w-6x1">
+  <iframe src="https://member-cd.web.app/?dojo=tachikawa" width="100%" height="100%" id="member-frame" frameborder="0" scrolling="yes" allowfullscreen></iframe>
+</div> 
 </div>
 </main>
 </template>
