ca0b9d865315e4e6d83a9bb02766b66da19192cb
I.KANO <ichiro.kano2@asahi.email.ne.jp>
2021-06-19 18:58:58 +0900

    Feature/addiframe (#31)
    * 練習
    
    * マルチアイコン(16x16, 32x32）を作成
    
    * メンター紹介の静的HTMLを削除

--- src/components/staffs/Members.vue
+++ src/components/staffs/Members.vue
@@ -1,39 +1,10 @@
 <template id="app">
 <main class='flex items-center'>
-<div class="support_back-container row">
-    <t-tag class='py-8 text-center font-semibold tracking-tight font-display text-white text-3xl ml-4'>
-        メンター紹介
-    </t-tag>
-    <br/>
-    <br/>
-<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-<div class="md:flex">
-  <img src="../../assets/members/inocchi.jpg" class="h-48 w-48 mt-10 ml-10 md:mb-10" />
-  <div class="p-10">
-    <div class="uppercase tracking-wide text-sm text-yellow-500 font-semibold">いのっち</div>
-    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-    <p class="mt-2 text-gray-600">普段はJavaを使った業務用のWEBアプリケーションの開発や保守をする仕事をしています。プログラミング教育活動に出会い、今は教育に関心があります。私はプログラミングを苦労して習得したので、これから学ぶ子どもたちには楽しく学べる場を提供したいと思い活動をしています。</p>
-  </div>
-</div>
-</div>
-<br/>
-<br/>
-<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-<div class="md:flex">
-  <img src="../../assets/members/inocchi.jpg" class="h-48 w-48 mt-10 ml-10 md:mb-10" />
-  <div class="p-10">
-    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">メンター２</div>
-    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Python C# C++</a>
-    <p class="mt-2 text-gray-600">かのさんの編集機能を追加予定Getting a new business off the ground is a lot of hard work. Getting a new business off the ground is a lot of hard work. Getting a new business off the ground is a lot of hard work. Getting a new business off the ground is a lot of hard work. Getting a new business off the ground is a lot of hard work. Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
-  </div>
-</div>
-</div> 
-<br/>
-<br/>
+
 <div class="w-screen min-h-300 bg-white">
   <iframe src="https://member-cd.web.app/?dojo=tachikawa" width="99%" id="member-frame" frameborder="0" scrolling="yes" allowfullscreen></iframe>
 </div> 
-</div>
+
 </main>
 </template>
 
