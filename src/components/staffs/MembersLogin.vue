87b7f8b6c10db70d071120d24014dba3bd581dc6
IchiroKano <ichiro.kano2@asahi.email.ne.jp>
2021-06-26 23:31:39 +0900

    不要ファイルを削除（Staff）

--- src/components/staffs/MembersLogin.vue
+++
@@ -1,9 +0,0 @@
-<template id="app">
-    <!-- メンバーを紹介するリスト -->
-    <div class="member-box">
-    <div id="member-list"></div>
-    <div class="member-tile-end"></div>
-    </div>
-    
-
-</template>
