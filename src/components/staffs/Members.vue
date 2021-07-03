c6889243cc6ec2495382cb65e0d41d616e79ab99
IchiroKano <ichiro.kano2@asahi.email.ne.jp>
2021-07-03 17:48:39 +0900

    サイズ調整(横100%, 高さ+余裕)

--- src/components/staffs/Members.vue
+++ src/components/staffs/Members.vue
@@ -3,7 +3,7 @@
 
   <!-- メンバー紹介サイトを組み込みます -->
   <div class="w-screen min-h-300 bg-white">
-    <iframe src="https://member-cd.web.app/?dojo=tachikawa" width="99%" id="member-frame" frameborder="0" scrolling="yes" allowfullscreen></iframe>
+    <iframe src="https://member-cd.web.app/?dojo=tachikawa" width="100%" id="member-frame" frameborder="0" scrolling="yes" allowfullscreen></iframe>
   </div> 
 
 </main>
@@ -19,7 +19,7 @@
   // 高さを自動調整します
   window.addEventListener('message', function(e) {
     if(e.origin=="https://member-cd.web.app"){
-      document.getElementById('member-frame').height = e.data + 120;
+      document.getElementById('member-frame').height = e.data + 180;
     }
   }, false);
 </script>
