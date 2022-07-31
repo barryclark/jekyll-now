f8cf536d90ace908a24065d25b58909cb489948b
purple2m <eosgg03@gmail.com>
2022-07-31 21:52:43 +0900

    ㅇㅇ

--- assets/normal.js
+++ assets/normal.js
@@ -130,6 +130,7 @@ if(searching_item){
       find_recipe_print += "<button value=\""+find_recipe[i]+"\"'>ふつう錬金"+find_recipe[i]+"</button>";
       find_recipe_print += "<a href='"+baseurl+"/"+lng+"/alchemist/?item="+find.name+"&recipe="+find_recipe[i]+"'>ふつう錬金"+find_recipe[i]+"</a>";
     } else {
+      find_recipe_print += "<button value=\""+find_recipe[i]+"\"'>연금"+find_recipe[i]+"</button>";
       find_recipe_print += "<a href='"+baseurl+"/ko/alchemist/?item="+find.name+"&recipe="+find_recipe[i]+"'>일반 연금"+find_recipe[i]+"</a>";
     }
   }
