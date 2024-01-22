a0941496c6193548fdf3d53f1f87d82c84c95f56
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2024-01-22 23:10:45 +0900

    Update update-folder-list.js

--- update-folder-list.js
+++ update-folder-list.js
@@ -4,24 +4,24 @@ const path = require('path');
 const folderPath = '_includes/_shared_block/'; // 업데이트하려는 폴더의 경로
 const outputPath = '_includes/_generated/tag.md'; // 결과를 저장할 Markdown 파일 경로
 
-function readFilesInDirectory(directoryPath) {
-  const files = fs.readdirSync(directoryPath);
-
+// folderPath아래의 모든 폴더를 탐색하며 파일 목록을 반환하는 함수
+// 디렉토리를 포함하지 않고 파일의 이름만 반환함
+function readFilesInDirectory(dir) {
+  const files = fs.readdirSync(dir);
   const fileList = [];
-  files.forEach((file) => {
-    const filePath = path.join(directoryPath, file);
-    const stats = fs.statSync(filePath);
-    if (stats.isFile()) {
-      fileList.push(`- ${filePath}`);
-    } else if (stats.isDirectory()) {
-      fileList.push(`# ${filePath}`);
-      fileList.push(...readFilesInDirectory(filePath)); // 하위 디렉토리 탐색
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      fileList.push(...readFilesInDirectory(filePath));
+    } else {
+      fileList.push(file);
     }
   });
-
   return fileList;
 }
 
+// 폴더 목록을 업데이트하고 결과를 출력하는 함수
 try {
   const fileList = readFilesInDirectory(folderPath).join('\n');
   fs.writeFileSync(outputPath, fileList, 'utf-8');
