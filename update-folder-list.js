de54374aa0c9886708e222ae1fc843f57a45d3db
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2024-01-22 23:12:58 +0900

    Update update-folder-list.js

--- update-folder-list.js
+++ update-folder-list.js
@@ -1,11 +1,17 @@
+/**
+ * 폴더 목록을 업데이트하는 스크립트
+ */
+
 const fs = require('fs');
 const path = require('path');
 
 const folderPath = '_includes/_shared_block/'; // 업데이트하려는 폴더의 경로
 const outputPath = '_includes/_generated/tag.md'; // 결과를 저장할 Markdown 파일 경로
 
-// folderPath아래의 모든 폴더를 탐색하며 파일 목록을 반환하는 함수
-// 디렉토리를 포함하지 않고 파일의 이름만 반환함
+/**
+ * folderPath아래의 모든 폴더를 탐색하며 파일 목록을 반환하는 함수
+ * 디렉토리를 포함하지 않고 파일의 이름만 반환함
+ */
 function readFilesInDirectory(dir) {
   const files = fs.readdirSync(dir);
   const fileList = [];
@@ -21,7 +27,9 @@ function readFilesInDirectory(dir) {
   return fileList;
 }
 
-// 폴더 목록을 업데이트하고 결과를 출력하는 함수
+/**
+ * 폴더 목록을 업데이트하고 결과를 출력하는 함수
+ */ 
 try {
   const fileList = readFilesInDirectory(folderPath).join('\n');
   fs.writeFileSync(outputPath, fileList, 'utf-8');
