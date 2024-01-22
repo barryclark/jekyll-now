1dcce6f8939f39a00b95cf73ec7f94a45ae7b4f2
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2024-01-22 22:43:51 +0900

    Update update-folder-list.js

--- update-folder-list.js
+++ update-folder-list.js
@@ -1,8 +1,7 @@
-//const fs = require('fs');
+const fs = require('fs');
 //const path = require('path');
 
 const folderPath = '_includes/_shared_block/'; // 업데이트하려는 폴더의 경로
 const outputPath = '_includes/_generated/tag.md'; // 결과를 저장할 Markdown 파일 경로
 
-const fsPromises = require('fs').promises
-fsPromises.writeFile('/path/to/file.txt', 'data to write')
+await fs.writeFileSync(outputPath, 'test'); // 결과를 저장할 Markdown 파일 생성
