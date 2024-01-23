85a1b12f48b7199ac967b261ccb2b20b6d7c0387
Muntean-Alin <alin@whitecitycode.com>
2024-01-23 10:28:06 +0200

    feat:add astro config for deploy with custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://coderdojo.ai3.ro',
   integrations: [
     tailwind(),
     react({
